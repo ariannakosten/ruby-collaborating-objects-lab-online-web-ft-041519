e2a540083b44c81c959734b93812918534068d8d
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-11 09:25:30 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -4,6 +4,7 @@ class MP3Importer
   
   def initialize(path) 
     @path = path
+    @files = 
   end
   
   def files
@@ -20,27 +21,6 @@ class MP3Importer
 
    def import
     @files.each {|file| Song.new_by_filename(file)}
-  end
-end
-class MP3Importer
-  attr_accessor :path
-
-  def initialize(path)
-    @path = path
-  end
-
-  def files
-    files = []
-    Dir.new(self.path).each do |file|
-      files << file if file.length > 4
-    end
-    files
-  end
-
-  def import
-    self.files.each do |filename|
-      Song.new_by_filename(filename)
-    end
   end
 end
 
