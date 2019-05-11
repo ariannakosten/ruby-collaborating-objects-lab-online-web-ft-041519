f07a36ac8dd906f9f70130e566c9ceaa2df3a4d9
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-11 09:26:02 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -6,18 +6,6 @@ class MP3Importer
     @path = path
     @files = Dir.entries(path).grep(/.*\.mp3/)
   end
-  
-  def files
-  
-end
-end
-class MP3Importer
-  attr_reader :path, :files
-
-   def initialize(path)
-    @path = path
-    @files = Dir.entries(path).grep(/.*\.mp3/)
-  end
 
    def import
     @files.each {|file| Song.new_by_filename(file)}
