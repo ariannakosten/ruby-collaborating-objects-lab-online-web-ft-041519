f4c60b5b12b448e99dd0eaf1b8363ccade9b27ca
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-11 09:44:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,16 +7,15 @@ class Song
     @artist = artist
   end
   
+  
+  
   def self.new_by_filename(file_name)  
     new_song = file_name.chomp(".mp3").split(" - ")
     new_song.name = Song.new_song[1]
     new_song.artist = new_song[0]
     new_song   
   end
-def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
-  end
+
 
   def self.new_by_filename(file)
     song_info = file.chomp(".mp3").split(" - ")
