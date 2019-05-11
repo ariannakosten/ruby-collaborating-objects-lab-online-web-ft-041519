cdd630717b2622dc202a45deffef790c944f61cc
Arianna Kosten <arianna.kosten@gmail.com>
2019-05-11 09:51:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,30 +4,27 @@ class Song
 
   def initialize(name)
     @name = name
-   
+   #@artist = nil
   end
   
-  # def artist_name=(name)
-  #   self.artist = Artist.find_or_create_by_name(name)
-  #   artist.add_song(self)
-  # end
+
   
-  # def self.new_by_filename(file_name)  
-  #   file = file_name.chomp(".mp3").split(" - ")
-  #   new_song.name = Song.new(file[1])
-  #   new_song.artist = file[0]
-  #   new_song   
-  # end
-
-def artist_name=(name)
-    self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+  def self.new_by_filename(file_name)  
+    file = file_name.chomp(".mp3").split(" - ")
+    new_song = Song.new(file[1])
+    new_song.artist = file[0]
+    new_song   
   end
 
-  def self.new_by_filename(file)
-    song_info = file.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
-    song.artist_name = song_info[0]
-    song
-  end
+# def artist_name=(name)
+#     self.artist = Artist.find_or_create_by_name(name)
+#     artist.add_song(self)
+#   end
+
+#   def self.new_by_filename(file)
+#     song_info = file.chomp(".mp3").split(" - ")
+#     song = Song.new(song_info[1])
+#     song.artist_name = song_info[0]
+#     song
+#   end
 end
